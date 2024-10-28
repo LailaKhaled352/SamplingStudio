b41198cd6222b16a33641ced8189f3e0a4dfe8e2
FatmaElsharkawy <fatmasaid462@yahoo.com>
2024-10-29 01:37:17 +0300

    composer done

---
+++ ComposedSignal.py
@@ -0,0 +1,55 @@
+import numpy as np
+from Sinusoid import Sinusoid
+class ComposedSignal:
+    composed_signals_list=[] 
+    selected_index= 0
+    def __init__(self):
+        self.component_list_numpy=[]
+        self.component_list_sinusoid=[]
+        self.signal_data_time= np.linspace(0,20,1000)
+        self.composed_signal=None
+
+    def compose_signal(self):
+        self.composed_signal= np.sum(self.component_list_numpy, axis=0)
+    
+    def add_component(self, freq, amp, phase, attr_list):
+        sinusoid= Sinusoid(freq, amp, phase)
+        self.component_list_sinusoid.append(sinusoid)
+
+        sinusoid_generated= sinusoid.generate_sinusoid(self.signal_data_time)
+        self.component_list_numpy.append(sinusoid_generated)
+
+        self.compose_signal()
+        attr_list.addItem(f"Component_n: amplitude= {amp}, freq= {freq}, phase= {phase}")
+    @classmethod
+    def remove_component(cls,component_list_widget, index):
+        selected_signal= cls.composed_signals_list[cls.selected_index]
+        _=selected_signal.component_list_numpy.pop(index)
+        _=selected_signal.component_list_sinusoid.pop(index)
+        selected_signal.compose_signal()
+        component_list_widget.takeItem(index)
+   
+    @classmethod
+    def show_components(cls, component_list_widget, index): #index used to know which signal to show its components 
+        selected_signal= cls.composed_signals_list[index]
+        cls.selected_index=index
+        for comp in selected_signal.component_list_sinusoid:
+            component_list_widget.addItem(f"Component_n: amplitude= {comp.amp}, frequency= {comp.freq}, phase= {comp.phase}")
+    
+    def save_signal(self, signals_List):
+        ComposedSignal.composed_signals_list.append(self)
+        print(len(self.component_list_sinusoid))
+        print(len(ComposedSignal.composed_signals_list))
+        signals_List.addItem(f"Composed_signal {len(ComposedSignal.composed_signals_list)}")
+   
+    @classmethod
+    def remove_signal(cls,signals_List_widget, index):
+        _= cls.composed_signals_list.pop(index) #remove composedSignal object
+        signals_List_widget.takeItem(index)
+
+    def get_composed_signal(self):
+        return self.composed_signal
+
+    def load_composed_signal(self):
+        signal_data_time, signal_data_amplitude= self.t_window, self.composed_signal
+        return  signal_data_time, signal_data_amplitude
--- MainWindow.py
+++ MainWindow.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication,QPushButton, QWidget, QSlider, QRadioButton, QComboBox, QTableWidget, QTableWidgetItem, QCheckBox,QMenu,QTextEdit, QDialog, QFileDialog, QInputDialog, QSizePolicy,QScrollArea,QVBoxLayout,QHBoxLayout
+from PyQt5.QtWidgets import QMainWindow, QApplication,QPushButton,QListWidget ,QSpinBox, QWidget, QSlider, QRadioButton, QComboBox, QTableWidget, QTableWidgetItem, QCheckBox,QMenu,QTextEdit, QDialog, QFileDialog, QInputDialog, QSizePolicy,QScrollArea,QVBoxLayout,QHBoxLayout
 from PyQt5.uic import loadUi
 from PyQt5.QtGui import QCursor,QBrush
 from PyQt5.QtCore import QPoint
@@ -7,12 +7,15 @@ import pyqtgraph as pg
 from Graph import Graph
 from Signal import Signal
 from Load import Load
+from ComposedSignal import ComposedSignal
+import numpy as np
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         loadUi("SamplingStudio.ui", self)
         self.signal=None
+        self.composed_signal_instance= ComposedSignal()
 
         #Laila
             # Initialize the existing PlotWidget
@@ -37,6 +40,80 @@ class MainWindow(QMainWindow):
 
         self.load_instance = Load()  # Instance of the Load class
 
+        #Fatma
+        self.add_component_button= self.findChild(QPushButton, 'addComponent')
+        self.frequency_entry= self.findChild(QSpinBox, 'freqSpinBox')
+        self.phase_entry= self.findChild(QSpinBox, 'phaseSpinBox')
+        self.amplitude_entry= self.findChild(QSpinBox, 'ampSpinBox')
+        self.frequency_entry.setValue(1)
+        self.phase_entry.setValue(0)
+        self.amplitude_entry.setValue(1)
+        self.add_component_button.clicked.connect(self.add_component)
+
+        self.signals_List = self.findChild(QListWidget, 'signalsList')
+        self.components_List = self.findChild(QListWidget, 'componList')
+        self.attr_List = self.findChild(QListWidget, 'attrList')
+
+        self.save_signal_button= self.findChild(QPushButton, 'GenerateButton')
+        self.save_signal_button.clicked.connect(self.save_signal)
+
+    #Fatma
+    def add_component(self):
+        freq= self.frequency_entry.value() 
+        amp=self.amplitude_entry.value()
+        phase=np.deg2rad(self.phase_entry.value())
+        self.composed_signal_instance.add_component(freq=freq, amp=amp, phase=phase,attr_list=self.attr_List)
+    
+    def save_signal(self):
+        self.attr_List.clear()
+        self.composed_signal_instance.save_signal(self.signals_List) #save this current instance
+        self.composed_signal_instance= ComposedSignal() #create a new instance to use here for next added components
+
+    def create_context_menu(self, position):
+        context_menu = QMenu(self)
+        context_menu.setStyleSheet("""
+        QMenu {
+            color: white;  /* Default text color */
+        }
+        QMenu::item::selected {  /* Hover state */
+            color: black;  /* Text color when hovering */
+            background-color: #a5a5a5;  /* Optional background color on hover */
+        } """)
+
+        if self.signals_List.underMouse():
+            signals_List_widget = self.signals_List
+            selected_row = signals_List_widget.selectionModel().currentIndex().row()
+            remove_signal_action = context_menu.addAction("Remove Signal")
+            remove_signal_action.triggered.connect(lambda: ComposedSignal.remove_signal(signals_List_widget, selected_row))
+
+            show_components_action = context_menu.addAction("Show Components")
+            show_components_action.triggered.connect(lambda: ComposedSignal.show_components(self.components_List, selected_row))
+
+            
+            load_signal_action = context_menu.addAction("Load Signal")
+            load_signal_action.triggered.connect(lambda: self.load_composed_signal(selected_row))
+        
+        elif self.components_List.underMouse():
+            components_list_widget= self.components_List
+            selected_row = components_list_widget.selectionModel().currentIndex().row() 
+            remove_component_action = context_menu.addAction("Remove Component")
+            remove_component_action.triggered.connect(lambda: ComposedSignal.remove_component(components_list_widget, selected_row))
+        else:
+            return         
+ 
+        # Get the global position of the cursor
+        cursor_position = QCursor.pos()
+        context_menu.exec_(cursor_position)
+
+    
+    def contextMenuEvent(self, event):
+        self.create_context_menu(event.pos())
+
+    def load_composed_signal(self, index):
+        selected_signal= ComposedSignal.composed_signals_list[index]
+        signal_data_time, signal_data_amplitude= selected_signal.load_composed_signal()
+        #Code to be added to show the sampled signal on graph1 
+    
     def show_default(self):
         self.signal = Signal(graph_num=1)
         self.graph1.set_signal(self.signal.signal_data_time, self.signal.signal_data_amplitude)
